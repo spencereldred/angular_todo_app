60f39eb1577f2d55913c9462785ae1b6d0740993
Spencer Eldred <eldredspencer@gmail.com>
2014-10-04 12:53:14 -1000

    Add alphabetical sort to both lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-Todo.create!(description: "Feed the cats", done: false)
-Todo.create!(description: "Go to Costco", done: false)
-Todo.create!(description: "Go to Recycle Center", done: false)
-Todo.create!(description: "Create one page app", done: true)
+Todo.create([
+{description: "Feed the cats", done: false},
+{description: "Go to Costco", done: false},
+{description: "Go to Recycle Center", done: false},
+{description: "Create one page app", done: true}
+])
